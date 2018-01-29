b5fe9536515e246dc2f336413ccf567096fd6902
Hashworks <awadh.kishor@hashworks.co>
2018-01-29 12:53:55 +0530

    code optimization

--- _site/assets/js/demo.js
+++ _site/assets/js/demo.js
@@ -1,13 +1,4 @@
-/**
- * demo.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2017, Codrops
- * http://www.codrops.com
- */
+
 {
 	setTimeout(() => document.body.classList.add('render'), 60);
 	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
